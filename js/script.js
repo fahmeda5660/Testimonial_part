d2e5ef2b3d5b2a9e23f06b74175c8f4c5df49b9e
Fahmeda Hasan Fariha <58398404+fahmedahasanfariha@users.noreply.github.com>
2021-06-08 21:58:27 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$('.port_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 500,
+    arrows: true,
+    prevArrow: '<i class="fa fa-chevron-left prev"></i>',
+    nextArrow: '<i class="fa fa-chevron-right next"></i>',
+  });
+  
